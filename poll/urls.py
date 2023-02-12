b5e82ed6eba8ba3c3d6d8649001374e257986930
Adhinesh-Reddy <68197578+Adhinesh-Reddy@users.noreply.github.com>
2023-02-12 15:51:31 -0500

    Update urls.py

--- poll/urls.py
+++ poll/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from questions.views import view
+from questions.views import login_view
 
 
 urlpatterns = [
