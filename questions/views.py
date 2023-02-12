130be2f6e1dec8a525b8affd91f3e3817c747f09
Adhinesh-Reddy <68197578+Adhinesh-Reddy@users.noreply.github.com>
2023-02-12 15:55:22 -0500

    Update views.py

--- questions/views.py
+++ questions/views.py
@@ -1,18 +1,6 @@
-from cmath import log
-from django.shortcuts import redirect, render
-
-from questions.models import Answers, Question
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from django.views.decorators.csrf import csrf_exempt
-# Create your views here.
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate , login
 from django.http import HttpResponse
 
 
-from django.contrib import messages
 def login_view(request):
     return HttpResponse("<h1>My first Django App!  yey</h1>")
    
